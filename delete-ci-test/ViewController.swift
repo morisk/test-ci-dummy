98ebe852688d788041f194795dafc33e9ada3071
Kramer Moris <moris@moblin.com>
2015-07-08 16:27:23 +0300

    Initial Commit

---
+++ delete-ci-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  delete-ci-test
+//
+//  Created by Moris Kramer on 8/7/15.
+//  Copyright © 2015 Moblin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ delete-ci-testTests/delete_ci_testTests.swift
@@ -0,0 +1,35 @@
+//
+//  delete_ci_testTests.swift
+//  delete-ci-testTests
+//
+//  Created by Moris Kramer on 8/7/15.
+//  Copyright © 2015 Moblin. All rights reserved.
+//
+
+import XCTest
+
+class delete_ci_testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
