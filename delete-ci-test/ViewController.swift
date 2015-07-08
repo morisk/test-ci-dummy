ca87f7b00a1eb46b396e22b286038c52e9ff0cb6
Kramer Moris <moris@moblin.com>
2015-07-08 16:34:58 +0300

    initial commit

--- delete-ci-test/ViewController.swift
+++ delete-ci-test/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var myLabel: UILabel!
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -20,6 +21,12 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  @IBAction func buttonTapped(sender: AnyObject) {
+    myLabel.text = "button"
+  }
 
+  @IBAction func nextScreenTapped(sender: AnyObject) {
+    
+  }
 }
 
